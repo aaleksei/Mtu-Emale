596836b78b8991dceffa5603e54c93f50a34be66
1 <raamatkeijo@gmail.com>
2020-03-04 17:00:47 +0200

    started auto gen occupations list

--- stat_parser.js
+++ stat_parser.js
@@ -1,14 +1,23 @@
+const GENDER = {
+  MEN:2,
+  MEN_AND_WOMEN:1,
+  WOMEN:3
+}
+const CENTRAL_TENDENCY = {
+  MEAN: 2,
+  AVERAGE: 1
+}
 fetch('http://andmebaas.stat.ee/sdmx-json/data/PA627')
   .then(function (response) {
     return response.json();
   })
     .then(function (data) {
-      let positions_ids = [4, 6, 30]
+      let positions_ids = [39, 36, 8, 35, 23, 32, 31, 6, 28, 11, 13, 28, 21, 32, 14, 21, 11, 37, 18, 41]
       let positions = data.structure.dimensions.series[0].values
       positions.forEach((position) => {
         positions_ids.forEach((positions_id) => {
           if (position.id == positions_id) {
-            console.log(position);
+            console.log(position.name);
           }
         });
       });
@@ -16,3 +25,7 @@ fetch('http://andmebaas.stat.ee/sdmx-json/data/PA627')
   .catch(function (err) {
     console.log(err);
 });
+
+function appendData (data){
+  var maniContainer = document.getElementById("")
+}
