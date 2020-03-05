cc4b16f07d64386c37fd357ca3d1bc50223c610f
1 <raamatkeijo@gmail.com>
2020-03-05 10:43:26 +0200

    Added job classification writing into Jobs column

--- stat_parser.js
+++ stat_parser.js
@@ -7,25 +7,42 @@ const CENTRAL_TENDENCY = {
   MEAN: 2,
   AVERAGE: 1
 }
+
 fetch('http://andmebaas.stat.ee/sdmx-json/data/PA627')
   .then(function (response) {
     return response.json();
   })
     .then(function (data) {
-      let positions_ids = [39, 36, 8, 35, 23, 32, 31, 6, 28, 11, 13, 28, 21, 32, 14, 21, 11, 37, 18, 41]
+      const positions_ids = [39, 36, 8, 35, 23, 32, 31, 6, 28, 11, 13, 28, 21, 32, 14, 21, 11, 37, 18, 41]
       let positions = data.structure.dimensions.series[0].values
+      let positionsDescriptions = []
       positions.forEach((position) => {
+        //console.log("id ",position.id)
+        let classifier = position.name.substr(0,2)
+        //console.log("klassifitsiaator ", classifier);
         positions_ids.forEach((positions_id) => {
           if (position.id == positions_id) {
             console.log(position.name);
+            positionsDescriptions.push(position)
           }
         });
       });
+      console.log(positionsDescriptions);
+      positionsDescriptions.forEach((position) => {
+        appendData(position)
+      });
+
+
     })
   .catch(function (err) {
     console.log(err);
 });
 
 function appendData (data){
-  var maniContainer = document.getElementById("")
+  const mainContainer = document.getElementById("jobs")
+  let positionContainer = document.createElement('div')
+  positionContainer.classList.add('job')
+  positionContainer.innerHTML = '<div class="title">' + data.name + '</div>'
+  mainContainer.append(positionContainer)
+
 }
